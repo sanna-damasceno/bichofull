f7dcb4cc039c1e18300d58aaee27a08048176fa3
sanna-damasceno <damascenosanna@gmail.com>
2026-02-19 17:08:03 -0400

    Create BichoFullApplicationTests.java

---
+++ backend/src/test/java/com/bichofull/BichoFullApplicationTests.java
@@ -0,0 +1,11 @@
+package com.bichofull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BichoFullApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
