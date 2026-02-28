bb1f59c7e6de18f2cb7ada2174ecfa4f8686a83e
sanna-damasceno <damascenosanna@gmail.com>
2026-02-28 17:04:13 -0400

    test: use H2 profile for all tests

--- backend/src/test/java/com/bichofull/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/bichofull/backend/BackendApplicationTests.java
@@ -1,13 +1,14 @@
 package com.bichofull.backend;
 
+import org.springframework.test.context.ActiveProfiles;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
