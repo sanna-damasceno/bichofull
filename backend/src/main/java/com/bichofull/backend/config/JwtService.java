3d39fda6c3a9d93bbb97305b3f61e5e844bd787e
sanna-damasceno <damascenosanna@gmail.com>
2026-03-02 16:30:17 -0400

    refactor: enhance JwtService with token validation and claim extraction

--- backend/src/main/java/com/bichofull/backend/config/JwtService.java
+++ backend/src/main/java/com/bichofull/backend/config/JwtService.java
@@ -1,5 +1,6 @@
 package com.bichofull.backend.config;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -11,19 +12,48 @@ import java.util.Date;
 @Service
 public class JwtService {
 
-    private final String SECRET_KEY = "bichofull-secret-key-bichofull-secret-key";
+    private final String SECRET_KEY =
+            "bichofull-secret-key-bichofull-secret-key";
 
     private Key getSigningKey() {
         return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
     }
 
+    //Gerar token
     public String generateToken(String email) {
 
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .setExpiration(
+                        new Date(System.currentTimeMillis() + 1000 * 60 * 60)
+                )
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    //Extrair email do token
+    public String extractEmail(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    //Validar token
+    public boolean isTokenValid(String token) {
+        try {
+            extractAllClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    //Extrair todos os dados do token
+    private Claims extractAllClaims(String token) {
+
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
