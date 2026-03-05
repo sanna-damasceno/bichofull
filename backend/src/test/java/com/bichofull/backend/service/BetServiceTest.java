6a77119a7a7c6038dc8bade666630b90c57cc723
sanna-damasceno <damascenosanna@gmail.com>
2026-03-05 17:33:12 -0400

    feat: implement bet creation and unit tests for BetService

--- backend/src/test/java/com/bichofull/backend/service/BetServiceTest.java
+++ backend/src/test/java/com/bichofull/backend/service/BetServiceTest.java
@@ -3,6 +3,9 @@ package com.bichofull.backend.service;
 import com.bichofull.backend.dto.BetRequestDTO;
 import com.bichofull.backend.dto.BetResponseDTO;
 import com.bichofull.backend.enums.BetType;
+import com.bichofull.backend.model.User;
+import com.bichofull.backend.repository.UserRepository;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,22 +22,34 @@ class BetServiceTest {
     @Autowired
     private BetService betService;
 
+    @Autowired
+    private UserRepository userRepository;
+
     // Criar aposta com sucesso
     @Test
     void shouldCreateBetSuccessfully() {
+
+        User user = new User();
+        user.setEmail("teste@email.com");
+        user.setName("Teste");
+        user.setPasswordHash("123");
+        user.setBalance(BigDecimal.valueOf(100));
+
+        userRepository.save(user);
 
         BetRequestDTO request = new BetRequestDTO();
         request.setType(BetType.GROUP);
         request.setChosenNumber("12");
         request.setAmount(BigDecimal.valueOf(10));
 
-        BetResponseDTO bet = betService.createBetDTO(request, "larissa@email.com");
+        BetResponseDTO bet = betService.createBetDTO(request, "teste@email.com");
 
         assertNotNull(bet);
         assertEquals("12", bet.getChosenNumber());
         assertEquals(BigDecimal.valueOf(10), bet.getAmount());
     }
-    //Teste de erro (saldo insuficiente)
+
+    // Teste de erro (saldo insuficiente)
     @Test
     void shouldNotAllowBetWithInsufficientBalance() {
 
@@ -44,7 +59,7 @@ class BetServiceTest {
         request.setAmount(BigDecimal.valueOf(100000));
 
         assertThrows(RuntimeException.class, () -> {
-            betService.createBetDTO(request, "larissa@email.com");
+            betService.createBetDTO(request, "teste@email.com");
         });
     }
 }
